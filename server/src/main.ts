6ab31258f19c4540fea72eb151abb9068ac1625b
Jérémy Boureux <jeremyboureux@gmail.com>
2025-04-10 13:37:12 +0200

    [6] test CI (#15)

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.useGlobalFilters(new PrismaClientExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(4000);
 }
 
 bootstrap().catch((reason) => console.log(reason));
