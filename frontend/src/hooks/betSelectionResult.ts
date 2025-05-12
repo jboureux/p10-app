8c9cf01c383adffcfd8aae5a6d1d7783e7d85924
Evan Dalleau <evandalleaublop@gmail.com>
2025-05-12 06:02:44 +0200

    feat: connect bet with front (not finished)

---
+++ frontend/src/hooks/betSelectionResult.ts
@@ -0,0 +1,33 @@
+import { callAPI } from "../lib/api-client";
+import {
+  CreateBetsSelectionResults,
+  UpdateBetSelectionResult,
+} from "@/types/bets";
+
+class BetSelectionResultHelper {
+  private apiService: typeof callAPI;
+
+  constructor() {
+    this.apiService = callAPI;
+  }
+
+  create = async (data: CreateBetsSelectionResults) => {
+    await fetch("/api/bet-selection-result", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
+
+  update = async (data: UpdateBetSelectionResult) => {
+    await fetch("/api/bet-selection-result", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
+}
--- frontend/src/types/bets.ts
+++ frontend/src/types/bets.ts
@@ -1,3 +1,24 @@
-export interface BetsSelectionResults {
+export interface BetSelectionResult {
+  id: string;
+
+  pointP10: number;
+
+  user: string;
+
+  grand_prix: string;
+
+  grand_prix_pilote: string;
+}
+export interface CreateBetsSelectionResults {
+  userId: string;
+
+  grandPrixId: string;
+
+  grandPrixPiloteId: string;
+}
+
+export interface UpdateBetSelectionResult {
+  userId: string;
+
   pointP10: number;
 }
